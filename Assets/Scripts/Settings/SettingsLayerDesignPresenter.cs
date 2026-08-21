54f0e2c15bdabff243efe0c68b49e1370249c730
브라키오사우르스 <brachio44294@gmail.com>
2026-08-22 06:55:34 +0900

    Add settings layer prefab

---
+++ Assets/Scripts/Settings/SettingsLayerDesignPresenter.cs
@@ -0,0 +1,475 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
+
+namespace SheepSheepBurger.Settings
+{
+    [ExecuteAlways]
+    [DisallowMultipleComponent]
+    public sealed class SettingsLayerDesignPresenter : MonoBehaviour
+    {
+        [Header("디자인 프리셋")]
+        [SerializeField] private SettingsLayerDesignPreset preset;
+
+        [Header("Canvas")]
+        [SerializeField] private Canvas canvas;
+        [SerializeField] private CanvasScaler canvasScaler;
+
+        [Header("패널")]
+        [SerializeField] private Image backdrop;
+        [SerializeField] private RectTransform settingsButtonRoot;
+        [SerializeField] private Button settingsButton;
+        [SerializeField] private Image settingsButtonBackground;
+        [SerializeField] private Outline settingsButtonOutline;
+        [SerializeField] private TMP_Text settingsButtonText;
+        [SerializeField] private RectTransform panel;
+        [SerializeField] private Image panelBackground;
+        [SerializeField] private Outline panelOutline;
+        [SerializeField] private RectTransform innerBorder;
+        [SerializeField] private Image innerBorderImage;
+        [SerializeField] private Outline innerBorderOutline;
+        [SerializeField] private RectTransform titleBanner;
+        [SerializeField] private Image titleBackground;
+        [SerializeField] private Outline titleOutline;
+        [SerializeField] private TMP_Text titleText;
+
+        [Header("BGM 행")]
+        [SerializeField] private RectTransform bgmIcon;
+        [SerializeField] private Image bgmIconImage;
+        [SerializeField] private Outline bgmIconOutline;
+        [SerializeField] private RectTransform bgmSliderRoot;
+        [SerializeField] private Image bgmSliderTrack;
+        [SerializeField] private RectTransform bgmSliderTrackRect;
+        [SerializeField] private Image bgmSliderFill;
+        [SerializeField] private Image bgmSliderHandle;
+        [SerializeField] private Outline bgmSliderHandleOutline;
+        [SerializeField] private RectTransform bgmValueBox;
+        [SerializeField] private Image bgmValueBoxBackground;
+        [SerializeField] private Outline bgmValueBoxOutline;
+        [SerializeField] private TMP_Text bgmValueText;
+
+        [Header("효과음 행")]
+        [SerializeField] private RectTransform sfxIcon;
+        [SerializeField] private Image sfxIconImage;
+        [SerializeField] private Outline sfxIconOutline;
+        [SerializeField] private RectTransform sfxSliderRoot;
+        [SerializeField] private Image sfxSliderTrack;
+        [SerializeField] private RectTransform sfxSliderTrackRect;
+        [SerializeField] private Image sfxSliderFill;
+        [SerializeField] private Image sfxSliderHandle;
+        [SerializeField] private Outline sfxSliderHandleOutline;
+        [SerializeField] private RectTransform sfxValueBox;
+        [SerializeField] private Image sfxValueBoxBackground;
+        [SerializeField] private Outline sfxValueBoxOutline;
+        [SerializeField] private TMP_Text sfxValueText;
+
+        public SettingsLayerDesignPreset Preset => preset;
+
+        public void Bind(SettingsLayerDesignPreset designPreset,
+                         Canvas boundCanvas,
+                         CanvasScaler boundCanvasScaler,
+                         Image boundBackdrop,
+                         RectTransform boundSettingsButtonRoot,
+                         Button boundSettingsButton,
+                         Image boundSettingsButtonBackground,
+                         Outline boundSettingsButtonOutline,
+                         TMP_Text boundSettingsButtonText,
+                         RectTransform boundPanel,
+                         Image boundPanelBackground,
+                         Outline boundPanelOutline,
+                         RectTransform boundInnerBorder,
+                         Image boundInnerBorderImage,
+                         Outline boundInnerBorderOutline,
+                         RectTransform boundTitleBanner,
+                         Image boundTitleBackground,
+                         Outline boundTitleOutline,
+                         TMP_Text boundTitleText,
+                         RectTransform boundBgmIcon,
+                         Image boundBgmIconImage,
+                         Outline boundBgmIconOutline,
+                         RectTransform boundBgmSliderRoot,
+                         Image boundBgmSliderTrack,
+                         RectTransform boundBgmSliderTrackRect,
+                         Image boundBgmSliderFill,
+                         Image boundBgmSliderHandle,
+                         Outline boundBgmSliderHandleOutline,
+                         RectTransform boundBgmValueBox,
+                         Image boundBgmValueBoxBackground,
+                         Outline boundBgmValueBoxOutline,
+                         TMP_Text boundBgmValueText,
+                         RectTransform boundSfxIcon,
+                         Image boundSfxIconImage,
+                         Outline boundSfxIconOutline,
+                         RectTransform boundSfxSliderRoot,
+                         Image boundSfxSliderTrack,
+                         RectTransform boundSfxSliderTrackRect,
+                         Image boundSfxSliderFill,
+                         Image boundSfxSliderHandle,
+                         Outline boundSfxSliderHandleOutline,
+                         RectTransform boundSfxValueBox,
+                         Image boundSfxValueBoxBackground,
+                         Outline boundSfxValueBoxOutline,
+                         TMP_Text boundSfxValueText)
+        {
+            preset = designPreset;
+            canvas = boundCanvas;
+            canvasScaler = boundCanvasScaler;
+            backdrop = boundBackdrop;
+            settingsButtonRoot = boundSettingsButtonRoot;
+            settingsButton = boundSettingsButton;
+            settingsButtonBackground = boundSettingsButtonBackground;
+            settingsButtonOutline = boundSettingsButtonOutline;
+            settingsButtonText = boundSettingsButtonText;
+            panel = boundPanel;
+            panelBackground = boundPanelBackground;
+            panelOutline = boundPanelOutline;
+            innerBorder = boundInnerBorder;
+            innerBorderImage = boundInnerBorderImage;
+            innerBorderOutline = boundInnerBorderOutline;
+            titleBanner = boundTitleBanner;
+            titleBackground = boundTitleBackground;
+            titleOutline = boundTitleOutline;
+            titleText = boundTitleText;
+            bgmIcon = boundBgmIcon;
+            bgmIconImage = boundBgmIconImage;
+            bgmIconOutline = boundBgmIconOutline;
+            bgmSliderRoot = boundBgmSliderRoot;
+            bgmSliderTrack = boundBgmSliderTrack;
+            bgmSliderTrackRect = boundBgmSliderTrackRect;
+            bgmSliderFill = boundBgmSliderFill;
+            bgmSliderHandle = boundBgmSliderHandle;
+            bgmSliderHandleOutline = boundBgmSliderHandleOutline;
+            bgmValueBox = boundBgmValueBox;
+            bgmValueBoxBackground = boundBgmValueBoxBackground;
+            bgmValueBoxOutline = boundBgmValueBoxOutline;
+            bgmValueText = boundBgmValueText;
+            sfxIcon = boundSfxIcon;
+            sfxIconImage = boundSfxIconImage;
+            sfxIconOutline = boundSfxIconOutline;
+            sfxSliderRoot = boundSfxSliderRoot;
+            sfxSliderTrack = boundSfxSliderTrack;
+            sfxSliderTrackRect = boundSfxSliderTrackRect;
+            sfxSliderFill = boundSfxSliderFill;
+            sfxSliderHandle = boundSfxSliderHandle;
+            sfxSliderHandleOutline = boundSfxSliderHandleOutline;
+            sfxValueBox = boundSfxValueBox;
+            sfxValueBoxBackground = boundSfxValueBoxBackground;
+            sfxValueBoxOutline = boundSfxValueBoxOutline;
+            sfxValueText = boundSfxValueText;
+        }
+
+        [ContextMenu("Apply Settings Layer Design")]
+        public void ApplyDesign()
+        {
+            if (preset == null)
+            {
+                return;
+            }
+
+            ApplyCanvas();
+
+            if (preset.applyRectTransformLayout)
+            {
+                ApplyLayout();
+            }
+
+            ApplyText();
+            ApplyColors();
+            MarkDirty();
+        }
+
+        private void Awake()
+        {
+            ApplyDesign();
+        }
+
+        private void OnValidate()
+        {
+#if UNITY_EDITOR
+            EditorApplication.delayCall -= ApplyDesignDelayed;
+            EditorApplication.delayCall += ApplyDesignDelayed;
+#else
+            ApplyDesign();
+#endif
+        }
+
+#if UNITY_EDITOR
+        private void ApplyDesignDelayed()
+        {
+            EditorApplication.delayCall -= ApplyDesignDelayed;
+
+            if (this == null)
+            {
+                return;
+            }
+
+            ApplyDesign();
+        }
+#endif
+
+        private void ApplyCanvas()
+        {
+            if (canvas != null)
+            {
+                canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+                canvas.overrideSorting = true;
+                canvas.sortingOrder = preset.canvasSortingOrder;
+            }
+
+            if (canvasScaler != null)
+            {
+                canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+                canvasScaler.referenceResolution = preset.referenceResolution;
+                canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+                canvasScaler.matchWidthOrHeight = preset.matchWidthOrHeight;
+            }
+        }
+
+        private void ApplyLayout()
+        {
+            SettingsLayerDesignPreset.LayoutSettings layout = preset.layout;
+
+            if (backdrop != null)
+            {
+                SetStretch(backdrop.rectTransform, Vector2.zero, Vector2.zero);
+            }
+
+            SetTopRight(settingsButtonRoot, layout.settingsButtonMargin, layout.settingsButtonSize);
+            SetCenter(panel, layout.panelAnchoredPosition, layout.panelSize);
+
+            if (innerBorder != null)
+            {
+                SetStretch(innerBorder, layout.innerBorderInset, -layout.innerBorderInset);
+            }
+
+            SetCenter(titleBanner, layout.titleAnchoredPosition, layout.titleSize);
+
+            ApplyRowLayout(layout.rowStartY,
+                           layout,
+                           bgmIcon,
+                           bgmSliderRoot,
+                           bgmSliderTrackRect,
+                           bgmValueBox);
+
+            ApplyRowLayout(layout.rowStartY - layout.rowSpacing,
+                           layout,
+                           sfxIcon,
+                           sfxSliderRoot,
+                           sfxSliderTrackRect,
+                           sfxValueBox);
+        }
+
+        private static void ApplyRowLayout(float rowY,
+                                           SettingsLayerDesignPreset.LayoutSettings layout,
+                                           RectTransform icon,
+                                           RectTransform sliderRoot,
+                                           RectTransform sliderTrack,
+                                           RectTransform valueBox)
+        {
+            SetCenter(icon, new Vector2(layout.iconX, rowY), Vector2.one * layout.iconSize);
+            SetCenter(sliderRoot, new Vector2(layout.sliderX, rowY), layout.sliderSize);
+            SetCenter(sliderTrack, Vector2.zero, new Vector2(layout.sliderSize.x, layout.sliderTrackHeight));
+            SetCenter(valueBox, new Vector2(layout.valueBoxX, rowY), layout.valueBoxSize);
+        }
+
+        private void ApplyText()
+        {
+            TMP_FontAsset fontAsset = preset.fontAsset;
+            SettingsLayerDesignPreset.TextSettings textSettings = preset.text;
+            SettingsLayerDesignPreset.ColorSettings colorSettings = preset.colors;
+
+            ApplyText(titleText, fontAsset, textSettings.titleFontSize, colorSettings.titleText);
+            ApplyText(settingsButtonText, fontAsset, textSettings.settingsButtonFontSize, colorSettings.settingsButtonText);
+            ApplyText(bgmValueText, fontAsset, textSettings.valueFontSize, colorSettings.valueText);
+            ApplyText(sfxValueText, fontAsset, textSettings.valueFontSize, colorSettings.valueText);
+        }
+
+        private void ApplyColors()
+        {
+            SettingsLayerDesignPreset.ColorSettings colors = preset.colors;
+
+            SetImage(backdrop, colors.backdrop, true);
+            SetImage(settingsButtonBackground, colors.settingsButtonBackground, true);
+            SetOutline(settingsButtonOutline, colors.settingsButtonOutline, new Vector2(4f, -4f));
+            ApplyButtonColors(settingsButton,
+                              colors.settingsButtonBackground,
+                              colors.settingsButtonHighlighted,
+                              colors.settingsButtonPressed);
+            SetImage(panelBackground, colors.panelBackground, true);
+            SetOutline(panelOutline, colors.panelOutline, new Vector2(6f, -6f));
+            SetImage(innerBorderImage, new Color(colors.panelBackground.r,
+                                                 colors.panelBackground.g,
+                                                 colors.panelBackground.b,
+                                                 0.01f),
+                     false);
+            SetOutline(innerBorderOutline, colors.innerOutline, new Vector2(4f, -4f));
+            SetImage(titleBackground, colors.titleBackground, true);
+            SetOutline(titleOutline, colors.titleOutline, new Vector2(5f, -5f));
+
+            ApplyRowColors(bgmIconImage,
+                           bgmIconOutline,
+                           bgmSliderTrack,
+                           bgmSliderFill,
+                           bgmSliderHandle,
+                           bgmSliderHandleOutline,
+                           bgmValueBoxBackground,
+                           bgmValueBoxOutline,
+                           colors);
+
+            ApplyRowColors(sfxIconImage,
+                           sfxIconOutline,
+                           sfxSliderTrack,
+                           sfxSliderFill,
+                           sfxSliderHandle,
+                           sfxSliderHandleOutline,
+                           sfxValueBoxBackground,
+                           sfxValueBoxOutline,
+                           colors);
+        }
+
+        private static void ApplyRowColors(Image iconImage,
+                                           Outline iconOutline,
+                                           Image sliderTrack,
+                                           Image sliderFill,
+                                           Image sliderHandle,
+                                           Outline sliderHandleOutline,
+                                           Image valueBoxBackground,
+                                           Outline valueBoxOutline,
+                                           SettingsLayerDesignPreset.ColorSettings colors)
+        {
+            SetImage(iconImage, colors.iconBackground, true);
+            SetOutline(iconOutline, colors.iconOutline, new Vector2(4f, -4f));
+            SetImage(sliderTrack, colors.sliderTrack, true);
+            SetImage(sliderFill, colors.sliderFill, true);
+            SetImage(sliderHandle, colors.sliderHandle, true);
+            SetOutline(sliderHandleOutline, colors.sliderHandleOutline, new Vector2(4f, -4f));
+            SetImage(valueBoxBackground, colors.valueBoxBackground, true);
+            SetOutline(valueBoxOutline, colors.valueBoxOutline, new Vector2(4f, -4f));
+        }
+
+        private static void ApplyText(TMP_Text text,
+                                      TMP_FontAsset fontAsset,
+                                      float fontSize,
+                                      Color color)
+        {
+            if (text == null)
+            {
+                return;
+            }
+
+            if (fontAsset != null)
+            {
+                text.font = fontAsset;
+            }
+
+            text.fontSize = fontSize;
+            text.color = color;
+            text.alignment = TextAlignmentOptions.Center;
+            text.raycastTarget = false;
+        }
+
+        private static void SetImage(Image image, Color color, bool raycastTarget)
+        {
+            if (image == null)
+            {
+                return;
+            }
+
+            image.color = color;
+            image.raycastTarget = raycastTarget;
+        }
+
+        private static void SetOutline(Outline outline, Color color, Vector2 distance)
+        {
+            if (outline == null)
+            {
+                return;
+            }
+
+            outline.effectColor = color;
+            outline.effectDistance = distance;
+            outline.useGraphicAlpha = true;
+        }
+
+        private static void SetStretch(RectTransform rect, Vector2 offsetMin, Vector2 offsetMax)
+        {
+            if (rect == null)
+            {
+                return;
+            }
+
+            rect.anchorMin = Vector2.zero;
+            rect.anchorMax = Vector2.one;
+            rect.offsetMin = offsetMin;
+            rect.offsetMax = offsetMax;
+        }
+
+        private static void SetTopRight(RectTransform rect, Vector2 margin, Vector2 size)
+        {
+            if (rect == null)
+            {
+                return;
+            }
+
+            rect.anchorMin = new Vector2(1f, 1f);
+            rect.anchorMax = new Vector2(1f, 1f);
+            rect.pivot = new Vector2(1f, 1f);
+            rect.anchoredPosition = new Vector2(-margin.x, -margin.y);
+            rect.sizeDelta = size;
+        }
+
+        private static void SetCenter(RectTransform rect, Vector2 anchoredPosition, Vector2 size)
+        {
+            if (rect == null)
+            {
+                return;
+            }
+
+            rect.anchorMin = new Vector2(0.5f, 0.5f);
+            rect.anchorMax = new Vector2(0.5f, 0.5f);
+            rect.pivot = new Vector2(0.5f, 0.5f);
+            rect.anchoredPosition = anchoredPosition;
+            rect.sizeDelta = size;
+        }
+
+        private static void ApplyButtonColors(Button button, Color normal, Color highlighted, Color pressed)
+        {
+            if (button == null)
+            {
+                return;
+            }
+
+            ColorBlock colors = button.colors;
+            colors.normalColor = normal;
+            colors.highlightedColor = highlighted;
+            colors.selectedColor = highlighted;
+            colors.pressedColor = pressed;
+            colors.disabledColor = new Color(normal.r, normal.g, normal.b, 0.45f);
+            colors.colorMultiplier = 1f;
+            colors.fadeDuration = 0.06f;
+            button.colors = colors;
+        }
+
+        private void MarkDirty()
+        {
+#if UNITY_EDITOR
+            if (Application.isPlaying)
+            {
+                return;
+            }
+
+            EditorUtility.SetDirty(this);
+
+            if (gameObject.scene.IsValid())
+            {
+                EditorSceneManager.MarkSceneDirty(gameObject.scene);
+            }
+#endif
+        }
+    }
+}
---
+++ Assets/Scripts/Settings/SettingsLayerDesignPreset.cs
@@ -0,0 +1,189 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace SheepSheepBurger.Settings
+{
+    [CreateAssetMenu(menuName = "SheepSheepBurger/Settings/Layer Design Preset", fileName = "SettingsLayerDesignPreset")]
+    public sealed class SettingsLayerDesignPreset : ScriptableObject
+    {
+        [TextArea(3, 6)]
+        [Tooltip("설정 레이어 디자인 담당자가 볼 메모입니다.")]
+        public string memo =
+            "다른 씬 위에 얹는 설정 레이어 프리셋입니다. 위치/크기는 프리팹의 RectTransform을 직접 드래그해서 수정하는 방식을 기본으로 둡니다.";
+
+        [Header("Canvas")]
+        public Vector2 referenceResolution = new Vector2(1920f, 1080f);
+
+        [Range(0f, 1f)]
+        [Tooltip("0이면 너비 기준, 1이면 높이 기준으로 UI가 스케일됩니다.")]
+        public float matchWidthOrHeight = 0.5f;
+
+        [Tooltip("값이 클수록 다른 Canvas보다 앞쪽에 보입니다.")]
+        public int canvasSortingOrder = 500;
+
+        [Header("폰트")]
+        public TMP_FontAsset fontAsset;
+
+        [Header("적용 옵션")]
+        [InspectorName("RectTransform 위치/크기 자동 적용")]
+        [Tooltip("끄면 프리팹/씬에서 손으로 수정한 UI 위치와 크기를 유지합니다.")]
+        public bool applyRectTransformLayout = false;
+
+        [Header("위치 / 크기")]
+        public LayoutSettings layout = LayoutSettings.Default;
+
+        [Header("글자")]
+        public TextSettings text = TextSettings.Default;
+
+        [Header("색")]
+        public ColorSettings colors = ColorSettings.Default;
+
+        [Serializable]
+        public struct LayoutSettings
+        {
+            public Vector2 panelSize;
+            public Vector2 panelAnchoredPosition;
+            public Vector2 settingsButtonSize;
+            public Vector2 settingsButtonMargin;
+            public Vector2 innerBorderInset;
+            public Vector2 titleSize;
+            public Vector2 titleAnchoredPosition;
+            public float rowStartY;
+            public float rowSpacing;
+            public float iconX;
+            public float sliderX;
+            public float valueBoxX;
+            public float iconSize;
+            public Vector2 sliderSize;
+            public float sliderTrackHeight;
+            public float sliderHandleSize;
+            public Vector2 valueBoxSize;
+
+            public static LayoutSettings Default => new LayoutSettings
+            {
+                panelSize = new Vector2(1240f, 720f),
+                panelAnchoredPosition = Vector2.zero,
+                settingsButtonSize = new Vector2(118f, 118f),
+                settingsButtonMargin = new Vector2(48f, 44f),
+                innerBorderInset = new Vector2(48f, 48f),
+                titleSize = new Vector2(520f, 140f),
+                titleAnchoredPosition = new Vector2(0f, 330f),
+                rowStartY = 90f,
+                rowSpacing = 180f,
+                iconX = -420f,
+                sliderX = 60f,
+                valueBoxX = 500f,
+                iconSize = 118f,
+                sliderSize = new Vector2(650f, 96f),
+                sliderTrackHeight = 20f,
+                sliderHandleSize = 82f,
+                valueBoxSize = new Vector2(150f, 86f)
+            };
+        }
+
+        [Serializable]
+        public struct TextSettings
+        {
+            [Min(1f)] public float titleFontSize;
+            [Min(1f)] public float settingsButtonFontSize;
+            [Min(1f)] public float valueFontSize;
+
+            public static TextSettings Default => new TextSettings
+            {
+                titleFontSize = 70f,
+                settingsButtonFontSize = 44f,
+                valueFontSize = 46f
+            };
+        }
+
+        [Serializable]
+        public struct ColorSettings
+        {
+            public Color backdrop;
+            public Color panelBackground;
+            public Color panelOutline;
+            public Color innerOutline;
+            public Color titleBackground;
+            public Color titleOutline;
+            public Color titleText;
+            public Color settingsButtonBackground;
+            public Color settingsButtonHighlighted;
+            public Color settingsButtonPressed;
+            public Color settingsButtonOutline;
+            public Color settingsButtonText;
+            public Color iconBackground;
+            public Color iconOutline;
+            public Color sliderTrack;
+            public Color sliderFill;
+            public Color sliderHandle;
+            public Color sliderHandleOutline;
+            public Color valueBoxBackground;
+            public Color valueBoxOutline;
+            public Color valueText;
+
+            public static ColorSettings Default => new ColorSettings
+            {
+                backdrop = new Color(0.06f, 0.10f, 0.08f, 0.48f),
+                panelBackground = new Color(0.59f, 0.73f, 0.60f, 1f),
+                panelOutline = new Color(0.12f, 0.30f, 0.21f, 1f),
+                innerOutline = new Color(0.24f, 0.45f, 0.32f, 1f),
+                titleBackground = new Color(0.48f, 0.64f, 0.49f, 1f),
+                titleOutline = new Color(0.10f, 0.26f, 0.20f, 1f),
+                titleText = new Color(0.07f, 0.14f, 0.10f, 1f),
+                settingsButtonBackground = new Color(0.46f, 0.62f, 0.46f, 1f),
+                settingsButtonHighlighted = new Color(0.55f, 0.70f, 0.55f, 1f),
+                settingsButtonPressed = new Color(0.34f, 0.51f, 0.36f, 1f),
+                settingsButtonOutline = new Color(0.08f, 0.22f, 0.15f, 1f),
+                settingsButtonText = new Color(0.07f, 0.14f, 0.10f, 1f),
+                iconBackground = new Color(0.72f, 0.83f, 0.71f, 1f),
+                iconOutline = new Color(0.13f, 0.30f, 0.20f, 1f),
+                sliderTrack = new Color(0.31f, 0.45f, 0.31f, 1f),
+                sliderFill = new Color(0.16f, 0.36f, 0.23f, 1f),
+                sliderHandle = new Color(0.20f, 0.44f, 0.27f, 1f),
+                sliderHandleOutline = new Color(0.05f, 0.16f, 0.10f, 1f),
+                valueBoxBackground = new Color(0.76f, 0.86f, 0.75f, 1f),
+                valueBoxOutline = new Color(0.12f, 0.30f, 0.21f, 1f),
+                valueText = new Color(0.08f, 0.15f, 0.11f, 1f)
+            };
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                return;
+            }
+
+            EditorApplication.delayCall -= ApplyToOpenLayers;
+            EditorApplication.delayCall += ApplyToOpenLayers;
+        }
+
+        public void ApplyToOpenLayers()
+        {
+            EditorApplication.delayCall -= ApplyToOpenLayers;
+
+            if (this == null)
+            {
+                return;
+            }
+
+            SettingsLayerDesignPresenter[] presenters =
+                FindObjectsByType<SettingsLayerDesignPresenter>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+            for (int i = 0; i < presenters.Length; i++)
+            {
+                if (presenters[i] != null && presenters[i].Preset == this)
+                {
+                    presenters[i].ApplyDesign();
+                }
+            }
+        }
+#endif
+    }
+}
